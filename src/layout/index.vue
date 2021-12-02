cd9e6d3725b83b0ed88bde129fe9dcb3790caf75
shaohan0228 <shaohan0228@live.cn>
2021-12-02 22:35:42 +0800

    清除页面中的一些不必要元素： 1. 隐藏了Layout页面的Navbar、RightPanel、Settings、TagsView 2. 删除了home.vue中的GithubCorner

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -3,14 +3,7 @@
     <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
     <sidebar class="sidebar-container" />
     <div :class="{hasTagsView:needTagsView}" class="main-container">
-      <div :class="{'fixed-header':fixedHeader}">
-        <navbar />
-        <tags-view v-if="needTagsView" />
-      </div>
       <app-main />
-      <right-panel v-if="showSettings">
-        <settings />
-      </right-panel>
     </div>
     <!--  防止刷新后主题丢失  -->
     <Theme v-show="false" ref="theme" />
@@ -18,8 +11,7 @@
 </template>
 
 <script>
-import RightPanel from '@/components/RightPanel'
-import { AppMain, Navbar, Settings, Sidebar, TagsView } from './components'
+import { AppMain, Sidebar } from './components'
 import ResizeMixin from './mixin/ResizeHandler'
 import { mapState } from 'vuex'
 import Theme from '@/components/ThemePicker'
@@ -28,11 +20,7 @@ export default {
   name: 'Layout',
   components: {
     AppMain,
-    Navbar,
-    RightPanel,
-    Settings,
     Sidebar,
-    TagsView,
     Theme
   },
   mixins: [ResizeMixin],
